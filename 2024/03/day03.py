ad78614e2c039ec82a8cd00ba7fcfef647b15bdc
Nils Krueger <koschiwsf@gmail.com>
2024-12-10 17:10:33 +0000

    AoC2024 day 03 - 2

--- 2024/03/day03.py
+++ 2024/03/day03.py
@@ -5,10 +5,14 @@ f = open('input.txt', 'r')
 
 sum = 0
 
-for l in f:
-    lines = re.findall('mul[(]{1}[0-9]{1,3},[0-9]{1,3}[)]{1}', l)
+lines = " ".join(line.strip() for line in f)
 
-    for s in lines:
+dos = lines.split("do()")
+for item in dos:
+    clean_item = re.sub(r"don't[(][)].*", '', item)
+    muls = re.findall('mul[(]{1}[0-9]{1,3},[0-9]{1,3}[)]{1}', clean_item)
+
+    for s in muls:
         mul = list(map(int, s.strip('mul()').split(',')))
         sum += math.prod(mul)
 
