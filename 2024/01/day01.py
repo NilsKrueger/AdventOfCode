2e8552c73651b88403c0dc8aea7de4c3ebafb6cf
Nils Krueger <koschiwsf@gmail.com>
2024-12-10 13:53:50 +0000

    AoC2024 day 01 - 1

---
+++ 2024/01/day01.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data_df = pd.read_csv('input.csv', sep = " ", header = None)
+
+# sort the columns, index have to be reseted since it is part of the sort
+left = data_df[0].sort_values().reset_index(drop=True)
+right = data_df[1].sort_values().reset_index(drop=True)
+sum = 0
+
+# combine to new df with individual sorted values
+merged = pd.DataFrame({'left': left, 'right': right})
+
+# iterate over rows and create sum of all rows
+for row in merged.itertuples():
+    #print(row[1], row[2], abs(row[1] - row[2]))
+    sum = sum + abs(row[1] - row[2])
+
+print(sum)
