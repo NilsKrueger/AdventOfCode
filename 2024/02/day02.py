4352a366ac43a3d7b946d36ee496bfe69723b7a1
Nils Krueger <koschiwsf@gmail.com>
2024-12-10 15:48:21 +0000

    AoC2024 day 02 - 2

--- 2024/02/day02.py
+++ 2024/02/day02.py
@@ -2,35 +2,40 @@ import pandas as pd
 
 f = open('input.txt', 'r')
 sum = 0
+sum_good = 0
 
-for l in f:
-    list = [int(x) for x in l.split()]
-    print(list)
-
-    current = list[0]
+def is_safe(list):
     asc = list == sorted(list)
     dec = list == sorted(list, reverse = True)
 
     safe = True
-
     for i in range(0, len(list)-1):
-        next = list[i + 1]
+        dist = abs(list[i] - list[i+1])
 
-        # ascending
-        if asc:
-            if next - current < 1 or next - current > 3:
-                safe = False
-        # decending
-        elif dec:
-            if current - next < 1 or current - next > 3:
-                safe = False
-        else:
+        if (dist < 1 or dist > 3):
             safe = False
+    return safe and (asc or dec)
 
-        current = next
+for l in f:
+    list = [int(x) for x in l.split()]
+    print(list, end = "")
 
+    current = list[0]
+    safe = is_safe(list)
+
+    print(' safe', safe, end = "")
     if safe:
         sum += 1
 
-print(sum)
+    good = False
+    for i in range(len(list)):
+        l2 = list[:i] + list[i+1:]
+        if is_safe(l2):
+            good = True
+    if good:
+        sum_good += 1
+    print(' good', good)
+
+print('Safe ', sum)
+print('Good ', sum_good)
 f.close()
