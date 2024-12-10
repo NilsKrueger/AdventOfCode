2cb8b3c58a432f68724ef5c24bf0085a373bcf66
Nils Krueger <koschiwsf@gmail.com>
2024-12-10 13:57:32 +0000

    AoC2024 day 01 - 2

--- 2024/01/day01.py
+++ 2024/01/day01.py
@@ -6,6 +6,7 @@ data_df = pd.read_csv('input.csv', sep = " ", header = None)
 left = data_df[0].sort_values().reset_index(drop=True)
 right = data_df[1].sort_values().reset_index(drop=True)
 sum = 0
+sim = 0
 
 # combine to new df with individual sorted values
 merged = pd.DataFrame({'left': left, 'right': right})
@@ -15,4 +16,9 @@ for row in merged.itertuples():
     #print(row[1], row[2], abs(row[1] - row[2]))
     sum = sum + abs(row[1] - row[2])
 
-print(sum)
+    value = row[1]
+    count = len(merged[merged['right'] == value])
+    sim = sim + (value * count)
+
+print('Part 1: ', sum)
+print('Part 2: ', sim)
